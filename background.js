e9d426227b592582ed70426ffc73ee17c4f35b07
Daniel Golant <dgolant@gmail.com>
2017-10-27 11:16:39 -0400

    resolved icon blocker issues and added a handler for installation

--- background.js
+++ background.js
@@ -1,4 +1,10 @@
 // Check to see if I am running this locally for dev mode
+const SETTINGS_URL = "https://localhost:5000/settings";
+const REDIR_URL = 'https://hjckfmabbnhjkfejdmiecefhkbekkefe.chromiumapp.org';
+const WEB_AUTH_FLOW_URL_1 = "https://accounts.google.com/o/oauth2/auth?client_id=881057203535-i7f0fqflce385r6u0p41nvseto0k8gbk.apps.googleusercontent.com&redirect_uri=";
+const WEB_AUTH_FLOW_URL_2 = "&response_type=token&scope=https://www.googleapis.com/auth/userinfo.profile";
+
+
 function isDevMode() {
     return !('update_url' in chrome.runtime.getManifest());
 }
@@ -10,10 +16,102 @@ if (1 == 0 /*isDevMode()*/ ) {
     server_url = 'https://tabmailer-174400.appspot.com/linksforuser';
 }
 
+
+// All useless for now, need to standardize auth path
+// function authenticatedGET(url) {
+//     return new Promise((resolve, reject) => {
+//         var retry = true;
+
+//         function getTokenAndXhr() {
+
+//             // for some reason the actual redirectMethod returns an additional /, which
+//             // Google Console does not like, despite me registering that as a redirect URL...
+//             var redirURL = REDIR_URL;
+//             chrome.identity.launchWebAuthFlow({
+//                     url: WEB_AUTH_FLOW_URL_1 + redirURL + WEB_AUTH_FLOW_URL_2,
+//                     interactive: true
+//                 },
+//                 function(redirect_url) {
+//                     if (chrome.runtime.lastError) {
+//                         reject(chrome.runtime.lastError);
+//                         console.log(chrome.runtime.lastError);
+//                         return;
+//                     }
+
+//                     // As if this whole process wasn't a big enough pain in the ass,
+//                     // this is C/P'd from SO because parsing this shit is not something I felt like doing
+//                     // seriously FU google
+//                     var access_token = new URL(redirect_url).hash.split('&').filter(function(el) { if (el.match('access_token') !== null) return true; })[0].split('=')[1];
+
+//                     var fullUrl = url + "?google_auth_token=" + access_token;
+
+//                     var xhr = new XMLHttpRequest();
+//                     xhr.open("GET", fullUrl, true);
+//                     xhr.setRequestHeader('Authorization',
+//                         'Bearer ' + access_token);
+//                     xhr.setRequestHeader("Content-Type", "application/json");
+
+//                     xhr.onload = function() {
+//                         if (this.status === 401 && retry) {
+//                             // This status may indicate that the cached
+//                             // access token was invalid. Retry once with
+//                             // a fresh token.
+//                             retry = false;
+//                             chrome.identity.removeCachedAuthToken({ 'token': access_token },
+//                                 getTokenAndXhr);
+//                             return;
+//                         } else if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+//                             resolve(this.response);
+//                         } else if (this.readyState == XMLHttpRequest.DONE && this.status != 200) {
+//                             reject(this.status);
+//                         }
+//                     }
+//                     xhr.send();
+//                 });
+//         }
+//         getTokenAndXhr();
+
+//     });
+// }
+
+
+
+// var getSettings = new Promise((resolve, reject) => {
+
+
+//     var getSettings = authenticatedGET(SETTINGS_URL);
+
+//     getSettings.then(function(settingsValue) {
+//         resolve(settingsValue);
+//     }, function(rejectionReason) {
+//         console.log("Settings Fetch Failed");
+//         reject(false);
+//     });
+// })
+
+
+// const setSettings = function(settings) {
+//     chrome.storage.sync.set({ 'settings': settings }, function() {
+//         console.log("Settings saved locally");
+//     });
+// }
+
+
 chrome.runtime.onInstalled.addListener(function(object) {
-    chrome.tabs.create({ url: "https://tabmailer-174400.appspot.com/" }, function(tab) {
-        console.log("New tab launched with https://tabmailer-174400.appspot.com/");
-    });
+    if (chrome.runtime.OnInstalledReason.INSTALL === object.reason) {
+        chrome.tabs.create({ url: "https://tabmailer-174400.appspot.com/" }, function(tab) {
+            console.log("New tab launched with https://tabmailer-174400.appspot.com/");
+        });
+    }
+
+    // fix later
+    // getSettings
+    //     .then(function(settings) {
+    //         setSettings(settings)
+    //     })
+    //     .catch(function(error) {
+    //         console.log("Settings error hit top level");
+    //     });
 });
 
 
@@ -91,10 +189,6 @@ function displayCompletionMessage(completion, responseObject) {
     }
 };
 
-
-
-
-
 // callback = function (error, httpStatus, responseText);
 function authenticatedXhr(method, url, req_body, callback) {
     var retry = true;
@@ -103,7 +197,7 @@ function authenticatedXhr(method, url, req_body, callback) {
 
         // for some reason the actual redirectMethod returns an additional /, which
         // Google Console does not like, despite me registering that as a redirect URL...
-        var redirURL = 'https://hjckfmabbnhjkfejdmiecefhkbekkefe.chromiumapp.org';
+        var redirURL = REDIR_URL;
         chrome.identity.launchWebAuthFlow({
                 url: "https://accounts.google.com/o/oauth2/auth?client_id=881057203535-i7f0fqflce385r6u0p41nvseto0k8gbk.apps.googleusercontent.com&redirect_uri=" + redirURL + "&response_type=token&scope=https://www.googleapis.com/auth/userinfo.profile",
                 interactive: true
