fff8d1f78ee570e0413aec5cc948386ad29cc755
Daniel Golant <dgolant@gmail.com>
2017-11-01 21:57:40 -0400

    fixed an indent

--- scripts/background.js
+++ scripts/background.js
@@ -80,7 +80,8 @@ function authenticatedGET(url) {
                         }
                     }
                     xhr.send();
-                });
+                }
+            );
         }
         getTokenAndXhr();
 
